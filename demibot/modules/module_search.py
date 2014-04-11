8a95977c22bf9ed905dd2b9723065a8dceac6ebd
max demian <mikar@gmx.de>
2014-04-11 19:15:41 +0200

    yt test

--- demibot/modules/module_search.py
+++ demibot/modules/module_search.py
@@ -6,8 +6,9 @@ import re
 import os
 
 
-def get_cx(configdir, cx_id=None):
+def get_cx(cx_name, configdir):
     "Reads Google Search ID from a file."
+    cx_id = None
     try:
         authfile = os.path.join(configdir, "auth")
         with open(authfile) as f:
@@ -15,7 +16,7 @@ def get_cx(configdir, cx_id=None):
     except IOError:
         cx_id = None
     else:
-        cx_id = re.search("(?<=cx\s).*", authstr)
+        cx_id = re.search("(?<={}\s).*".format(cx_name), authstr)
         if cx_id:
             cx_id = cx_id.group()
 
@@ -24,7 +25,7 @@ def get_cx(configdir, cx_id=None):
 def command_g(bot, user, channel, args):
     "Searches Google and returns the first result. Usage: g <searchterm>"
 
-    cx = get_cx(bot.factory.configdir)
+    cx = get_cx("gcx", bot.factory.configdir)
     if not cx:
         return
 
@@ -47,3 +48,31 @@ def command_g(bot, user, channel, args):
     title = parsed["items"][0]["title"]
 
     bot.say(channel, "Google: {} - {}".format(title, first_url))
+
+
+def command_yt(bot, user, channel, args):
+    "Searches Youtube and returns the first result. Usage: g <searchterm>"
+
+    cx = get_cx("ytcx", bot.factory.configdir)
+    if not cx:
+        return
+
+    url = "https://www.googleapis.com/customsearch/v1?q=%s&cx=%s&num=1&safe"\
+          "=off&key=AIzaSyCaXV2IVfhG1lZ38HP7Xr9HzkGycmsuSDU"
+
+    if not args:
+        return bot.say(channel, "No search query!")
+
+    search = get_urlinfo(url % (args, cx))
+    parsed = search.json()
+
+    results = parsed["searchInformation"]["totalResults"]
+
+    if results == "0":
+        return bot.say(channel, "Nothing found for query: {}"
+                       .format(args))
+
+    first_url = parsed["items"][0]["link"]
+    title = parsed["items"][0]["title"]
+
+    bot.say(channel, "YouTube: {} - {}".format(title, first_url))
